23e8c3f3d04c04a11bbbd1b504577d53eb4e42e0
JadenKim-dev <jaden.kim5179@gmail.com>
2022-02-13 18:18:23 +0900

    first commit

---
+++ src/main/java/sada/sadamall/SadamallApplication.java
@@ -0,0 +1,13 @@
+package sada.sadamall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadamallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadamallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sada/sadamall/SadamallApplicationTests.java
@@ -0,0 +1,13 @@
+package sada.sadamall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadamallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
