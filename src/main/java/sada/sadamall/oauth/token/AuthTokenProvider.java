588ff8114db198cc2a5bf82bc6b7eb037a25a7b4
JadenKim-dev <jaden.kim5179@gmail.com>
2022-02-17 20:28:57 +0900

    feat(oauth) : getAuthentication

--- src/main/java/sada/sadamall/oauth/token/AuthTokenProvider.java
+++ src/main/java/sada/sadamall/oauth/token/AuthTokenProvider.java
@@ -1,11 +1,19 @@
 package sada.sadamall.oauth.token;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import sada.sadamall.oauth.exception.TokenValidFailedException;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Slf4j
 public class AuthTokenProvider {
@@ -25,5 +33,19 @@ public class AuthTokenProvider {
 
     public AuthToken createAuthTokenWithRole(String id, String role, Date expiry) {
         return new AuthToken(id, role, expiry, key);
+    }
+
+    public Authentication getAuthentication(AuthToken authToken) {
+        if(authToken.validate()) {
+            Claims claims = authToken.getTokenClaims();
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority(
+                    claims.get(AuthToken.AUTHORITIES_KEY).toString()
+            );
+            Collection<? extends SimpleGrantedAuthority> authorities = List.of(authority);
+            User principal = new User(claims.getSubject(), "", authorities);
+            return new UsernamePasswordAuthenticationToken(principal, authToken, authorities);
+        } else {
+            throw new TokenValidFailedException();
+        }
     }
 }
--- src/test/java/sada/sadamall/oauth/token/AuthTokenProviderTest.java
+++ src/test/java/sada/sadamall/oauth/token/AuthTokenProviderTest.java
@@ -5,16 +5,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import sada.sadamall.RandomString;
+import sada.sadamall.oauth.entity.RoleType;
+import sada.sadamall.oauth.exception.TokenValidFailedException;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class AuthTokenTest {
+public class AuthTokenProviderTest {
     AuthTokenProvider authTokenProvider;
 
     @BeforeEach
@@ -51,5 +60,37 @@ public class AuthTokenTest {
         assertThat(claims.getExpiration().getTime()).isEqualTo((nowTime + 100000)/1000*1000);
     }
 
+    @Test
+    public void testGetAuthentication() {
+        long nowTime = new Date().getTime();
+        AuthToken authToken = authTokenProvider.createAuthTokenWithRole(
+                "1",
+                RoleType.USER.getCode(),
+                new Date(nowTime + 100000)
+        );
+        Authentication authentication = authTokenProvider.getAuthentication(authToken);
+        User principal = (User) authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        GrantedAuthority authority = authorities.iterator().next();
+        Object credentials = authentication.getCredentials();
+
+        assertThat(authentication.isAuthenticated()).isTrue();
+        assertThat(principal.getUsername()).isEqualTo("1");
+        assertThat(principal.getPassword()).isEqualTo("");
+        assertThat(authority.getAuthority()).isEqualTo(RoleType.USER.getCode());
+        assertThat(credentials).isEqualTo(authToken);
+    }
+
+    @Test
+    public void testGetAuthentication_만료된_토큰() {
+        long nowTime = new Date().getTime();
+        AuthToken authToken = authTokenProvider.createAuthTokenWithRole(
+                "1",
+                RoleType.USER.getCode(),
+                new Date(nowTime - 100000)
+        );
+        assertThatThrownBy(() -> authTokenProvider.getAuthentication(authToken))
+                .isInstanceOf(TokenValidFailedException.class);
+    }
 
 }
