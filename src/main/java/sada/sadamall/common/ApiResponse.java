3ac175afaa78e9500956b86d620e6c1050aa3929
JadenKim-dev <jaden.kim5179@gmail.com>
2022-02-22 21:40:49 +0900

    feat(oauth) : ApiResponse

---
+++ src/main/java/sada/sadamall/common/ApiResponse.java
@@ -0,0 +1,55 @@
+package sada.sadamall.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+    public final static int SUCCESS = 200;
+    public final static int NOT_FOUND = 400;
+    public final static int FAILED = 500;
+    public final static String SUCCESS_MESSAGE = "SUCCESS";
+    public final static String NOT_FOUND_MESSAGE = "NOT FOUND";
+    public final static String FAILED_MESSAGE = "서버에서 오류가 발생하였습니다";
+    public final static String INVALID_ACCESS_TOKEN_MESSAGE = "Invalid access token.";
+    public final static String INVALID_REFRESH_TOKEN_MESSAGE = "Invalid refresh token.";
+    public final static String NOT_EXPIRED_TOKEN_YET_MESSAGE = "Not expired token yet.";
+
+    private final ApiResponseHeader header;
+    private final Map<String, T> body;
+
+    public static <T> ApiResponse<T> success(String name, T body) {
+        Map<String, T> map = new HashMap<>();
+        map.put(name, body);
+
+        return new ApiResponse<>(
+                new ApiResponseHeader(SUCCESS, SUCCESS_MESSAGE),
+                map
+        );
+    }
+
+    public static <T> ApiResponse<T> fail() {
+        return new ApiResponse<>(
+                new ApiResponseHeader(FAILED, FAILED_MESSAGE),
+                null
+        );
+    }
+
+    public static <T> ApiResponse<T> invalidAccessToken() {
+        return new ApiResponse<>(
+                new ApiResponseHeader(FAILED, INVALID_ACCESS_TOKEN_MESSAGE),
+                null
+        );
+    }
+
+    public static <T> ApiResponse<T> notExpiredTokenYet() {
+        return new ApiResponse<>(
+                new ApiResponseHeader(FAILED, NOT_EXPIRED_TOKEN_YET_MESSAGE),
+                null
+        );
+    }
+}
---
+++ src/main/java/sada/sadamall/common/ApiResponseHeader.java
@@ -0,0 +1,12 @@
+package sada.sadamall.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class ApiResponseHeader {
+    private int code;
+    private String message;
+}
