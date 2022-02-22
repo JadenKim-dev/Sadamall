34711c9ce0313db4fce143969aea1918d6160b69
JadenKim-dev <jaden.kim5179@gmail.com>
2022-02-22 20:39:20 +0900

    feat(oauth) : CorsProperties - ConfigurationProperties

--- src/main/java/sada/sadamall/SadamallApplication.java
+++ src/main/java/sada/sadamall/SadamallApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import sada.sadamall.config.properties.AppProperties;
+import sada.sadamall.config.properties.CorsProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
-		AppProperties.class
+		AppProperties.class,
+		CorsProperties.class
 })
 public class SadamallApplication {
 
