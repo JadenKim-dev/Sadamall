889a79afc67b9b948a3a1aec0b582e8948734ec0
JadenKim-dev <jaden.kim5179@gmail.com>
2022-02-23 21:05:33 +0900

    feat(oauth) : HeaderUtil

---
+++ src/test/java/sada/sadamall/utils/HeaderUtilTest.java
@@ -0,0 +1,62 @@
+package sada.sadamall.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class HeaderUtilTest {
+
+    private MockHttpServletRequest request;
+
+    @BeforeEach
+    public void beforeEach() {
+        request = new MockHttpServletRequest();
+    }
+
+    @Test
+    public void getAccessToken() {
+        //given
+        request.addHeader(
+                HeaderUtil.HEADER_AUTHORIZATION,
+                HeaderUtil.TOKEN_PREFIX + "result"
+        );
+
+        //when
+        String accessToken = HeaderUtil.getAccessToken(request);
+
+        //then
+        assertThat(accessToken).isEqualTo("result");
+    }
+
+    @Test
+    public void getAccessTokenWithNoAuthorization() {
+        //given
+
+        //when
+        String accessToken = HeaderUtil.getAccessToken(request);
+
+        //then
+        assertThat(accessToken).isNull();
+    }
+
+    @Test
+    public void getAccessTokenWithInvalidValue() throws Exception {
+        //given
+        request.addHeader(
+                HeaderUtil.HEADER_AUTHORIZATION,
+                "wrong result"
+        );
+        //when
+        String accessToken = HeaderUtil.getAccessToken(request);
+
+        //then
+        assertThat(accessToken).isNull();
+    }
+}
